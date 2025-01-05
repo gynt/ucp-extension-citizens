74b4b610212110f4bd21195efb9d35b0d0c90f04
Gynt <Gynt@users.noreply.github.com>
2025-01-05 20:56:13 +0100

    add jester

--- modifications/spawn.lua
+++ modifications/spawn.lua
@@ -13,28 +13,43 @@ return {
       --[[
         Remove child spawning
       --]]
-      core.writeCode(core.AOBScan("6A 40 51"), nops(30))
+      local childSpawnAddress = core.AOBScan("6A 40 51")
+      core.writeCode(childSpawnAddress, nops(30))
+      core.writeCode(childSpawnAddress, {core.AssemblyLambda("xor eax, eax", {})})
     end
 
     if config.disable.mother == true then
       --[[
       Remove mother spawning
       --]]
-      core.writeCode(core.AOBScan("6A 3F 51"), nops(30))
+      local motherSpawnAddress = core.AOBScan("6A 3F 51")
+      core.writeCode(motherSpawnAddress, nops(30))
+      core.writeCode(motherSpawnAddress, {core.AssemblyLambda("xor eax, eax", {})})
     end
 
     if config.disable.drunk == true then
       --[[
       Remove drunk spawning
       --]]
-      core.writeCode(core.AOBScan("6A 23 03 C9"), nops(29))
+      local drunkSpawnAddress = core.AOBScan("6A 23 03 C9")
+      core.writeCode(drunkSpawnAddress, nops(29))
+      core.writeCode(drunkSpawnAddress, {core.AssemblyLambda("xor eax, eax", {})})
     end
 
     if config.disable.chicken == true then
       --[[
       Remove chicken spawning
       --]]
-      core.writeCode(core.AOBScan("6A 3E 03 C9"), nops(29))
+      local chickenSpawnAddress = core.AOBScan("6A 3E 03 C9")
+      core.writeCode(chickenSpawnAddress, nops(29))
+      core.writeCode(chickenSpawnAddress, {core.AssemblyLambda("xor eax, eax", {})})
+    end
+
+    if config.disable.jester == true then
+      local jesterSpawnAddress = core.AOBScan("6A 39 52 8D 04 ED 00 00 00 00")
+      core.writeCode(jesterSpawnAddress, nops(31))
+      core.writeCode(jesterSpawnAddress, {core.AssemblyLambda("xor eax, eax", {})})
+
     end
   end,
 }
